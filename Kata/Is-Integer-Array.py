25ef9de3c4362ba83e86a92d167b8e8574a168c5
RODRIGO CORDEIRO <rodrigocordeiro@uni9.edu.br>
2020-01-02 18:09:04 -0300

    Initialize repo

--- Kata/Is-Integer-Array.py
+++ Kata/Is-Integer-Array.py
@@ -1,5 +1,43 @@
+# -*- coding: utf-8 -*-
+# Author: Rodrigo Cordeiro
+  # returns *True* if every element in an array is an integer or a float with no decimals.
+  # returns *True* if array is empty.
+  # returns *False* for every other input.
+
+teste1 = [1,2,3,4,5]
+teste2 = [1,2,3,"oi"]
+teste3 = [1,2,3,4,3.5]
+teste4 = []
+teste5 = [13,8,3.0]
 def is_int_array(arr):
+    for item in arr:
+    	x= type(item)
+    	if x == int:
+    		pass
+    	else:
+    		print(arr)
+    		print(arr.index(item),item)
+    		return False
     return True
-  * returns *True* if every element in an array is an integer or a float with no decimals.
-  * returns *True* if array is empty.
-  * returns *False* for every other input.
+
+def isfloat(arr):
+	for item in arr:
+		if type(item) == float and item > 0:
+			print(item,"OK")
+		else:
+			print(item)
+
+
+print(isfloat(teste3))
+print(isfloat(teste5))
+
+
+# print(is_int_array(teste1))
+
+# print(is_int_array(teste2))
+
+# print(is_int_array(teste3))
+
+# print(is_int_array(teste4))
+
+# print(is_int_array(teste5))
