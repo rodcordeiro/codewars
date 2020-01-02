6b556f55421d1f4c469588e46127b5606fba26a4
RODRIGO CORDEIRO <rodrigocordeiro@uni9.edu.br>
2020-01-02 17:37:39 -0300

    Initialize repo

---
+++ Kata/Is-Integer-Array.py
@@ -0,0 +1,5 @@
+def is_int_array(arr):
+    return True
+  * returns *True* if every element in an array is an integer or a float with no decimals.
+  * returns *True* if array is empty.
+  * returns *False* for every other input.
